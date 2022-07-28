f20008f480fa711ae56d41026aba2a811f27d312
Lucas Pícoli <109911121+Picoleh@users.noreply.github.com>
2022-07-28 20:43:19 -0300

    Add files via upload

---
+++ Escalator/Escalator/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Escalator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_C = new System.Windows.Forms.Label();
+            this.lbl_Cs = new System.Windows.Forms.Label();
+            this.lbl_Ds = new System.Windows.Forms.Label();
+            this.lbl_D = new System.Windows.Forms.Label();
+            this.lbl_E = new System.Windows.Forms.Label();
+            this.lbl_F = new System.Windows.Forms.Label();
+            this.lbl_Fs = new System.Windows.Forms.Label();
+            this.lbl_G = new System.Windows.Forms.Label();
+            this.lbl_Gs = new System.Windows.Forms.Label();
+            this.lbl_A = new System.Windows.Forms.Label();
+            this.lbl_As = new System.Windows.Forms.Label();
+            this.lbl_B = new System.Windows.Forms.Label();
+            this.lbl_Painel = new System.Windows.Forms.Label();
+            this.bt_buscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_C
+            // 
+            this.lbl_C.BackColor = System.Drawing.Color.Plum;
+            this.lbl_C.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_C.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_C.Location = new System.Drawing.Point(20, 115);
+            this.lbl_C.Name = "lbl_C";
+            this.lbl_C.Size = new System.Drawing.Size(80, 80);
+            this.lbl_C.TabIndex = 0;
+            this.lbl_C.Text = "C";
+            this.lbl_C.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_C.Click += new System.EventHandler(this.lbl_C_Click);
+            // 
+            // lbl_Cs
+            // 
+            this.lbl_Cs.BackColor = System.Drawing.Color.Plum;
+            this.lbl_Cs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Cs.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Cs.Location = new System.Drawing.Point(186, 115);
+            this.lbl_Cs.Name = "lbl_Cs";
+            this.lbl_Cs.Size = new System.Drawing.Size(80, 80);
+            this.lbl_Cs.TabIndex = 1;
+            this.lbl_Cs.Text = "C#";
+            this.lbl_Cs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Cs.Click += new System.EventHandler(this.lbl_Cs_Click);
+            // 
+            // lbl_Ds
+            // 
+            this.lbl_Ds.BackColor = System.Drawing.Color.Plum;
+            this.lbl_Ds.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Ds.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Ds.Location = new System.Drawing.Point(538, 115);
+            this.lbl_Ds.Name = "lbl_Ds";
+            this.lbl_Ds.Size = new System.Drawing.Size(80, 80);
+            this.lbl_Ds.TabIndex = 2;
+            this.lbl_Ds.Text = "D#";
+            this.lbl_Ds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Ds.Click += new System.EventHandler(this.lbl_Ds_Click);
+            // 
+            // lbl_D
+            // 
+            this.lbl_D.BackColor = System.Drawing.Color.Plum;
+            this.lbl_D.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_D.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_D.Location = new System.Drawing.Point(362, 115);
+            this.lbl_D.Name = "lbl_D";
+            this.lbl_D.Size = new System.Drawing.Size(80, 80);
+            this.lbl_D.TabIndex = 3;
+            this.lbl_D.Text = "D";
+            this.lbl_D.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_D.Click += new System.EventHandler(this.lbl_D_Click);
+            // 
+            // lbl_E
+            // 
+            this.lbl_E.BackColor = System.Drawing.Color.Plum;
+            this.lbl_E.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_E.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_E.Location = new System.Drawing.Point(714, 115);
+            this.lbl_E.Name = "lbl_E";
+            this.lbl_E.Size = new System.Drawing.Size(80, 80);
+            this.lbl_E.TabIndex = 4;
+            this.lbl_E.Text = "E";
+            this.lbl_E.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_E.Click += new System.EventHandler(this.lbl_E_Click);
+            // 
+            // lbl_F
+            // 
+            this.lbl_F.BackColor = System.Drawing.Color.Plum;
+            this.lbl_F.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_F.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_F.Location = new System.Drawing.Point(890, 115);
+            this.lbl_F.Name = "lbl_F";
+            this.lbl_F.Size = new System.Drawing.Size(80, 80);
+            this.lbl_F.TabIndex = 5;
+            this.lbl_F.Text = "F";
+            this.lbl_F.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_F.Click += new System.EventHandler(this.lbl_F_Click);
+            // 
+            // lbl_Fs
+            // 
+            this.lbl_Fs.BackColor = System.Drawing.Color.Plum;
+            this.lbl_Fs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Fs.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Fs.Location = new System.Drawing.Point(1056, 115);
+            this.lbl_Fs.Name = "lbl_Fs";
+            this.lbl_Fs.Size = new System.Drawing.Size(80, 80);
+            this.lbl_Fs.TabIndex = 6;
+            this.lbl_Fs.Text = "F#";
+            this.lbl_Fs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Fs.Click += new System.EventHandler(this.lbl_Fs_Click);
+            // 
+            // lbl_G
+            // 
+            this.lbl_G.BackColor = System.Drawing.Color.Plum;
+            this.lbl_G.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_G.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_G.Location = new System.Drawing.Point(1222, 115);
+            this.lbl_G.Name = "lbl_G";
+            this.lbl_G.Size = new System.Drawing.Size(80, 80);
+            this.lbl_G.TabIndex = 7;
+            this.lbl_G.Text = "G";
+            this.lbl_G.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_G.Click += new System.EventHandler(this.lbl_G_Click);
+            // 
+            // lbl_Gs
+            // 
+            this.lbl_Gs.BackColor = System.Drawing.Color.Plum;
+            this.lbl_Gs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Gs.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Gs.Location = new System.Drawing.Point(273, 245);
+            this.lbl_Gs.Name = "lbl_Gs";
+            this.lbl_Gs.Size = new System.Drawing.Size(80, 80);
+            this.lbl_Gs.TabIndex = 8;
+            this.lbl_Gs.Text = "G#";
+            this.lbl_Gs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Gs.Click += new System.EventHandler(this.lbl_Gs_Click);
+            // 
+            // lbl_A
+            // 
+            this.lbl_A.BackColor = System.Drawing.Color.Plum;
+            this.lbl_A.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_A.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_A.Location = new System.Drawing.Point(507, 245);
+            this.lbl_A.Name = "lbl_A";
+            this.lbl_A.Size = new System.Drawing.Size(80, 80);
+            this.lbl_A.TabIndex = 9;
+            this.lbl_A.Text = "A";
+            this.lbl_A.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_A.Click += new System.EventHandler(this.lbl_A_Click);
+            // 
+            // lbl_As
+            // 
+            this.lbl_As.BackColor = System.Drawing.Color.Plum;
+            this.lbl_As.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_As.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_As.Location = new System.Drawing.Point(744, 245);
+            this.lbl_As.Name = "lbl_As";
+            this.lbl_As.Size = new System.Drawing.Size(80, 80);
+            this.lbl_As.TabIndex = 10;
+            this.lbl_As.Text = "A#";
+            this.lbl_As.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_As.Click += new System.EventHandler(this.lbl_As_Click);
+            // 
+            // lbl_B
+            // 
+            this.lbl_B.BackColor = System.Drawing.Color.Plum;
+            this.lbl_B.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_B.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_B.Location = new System.Drawing.Point(971, 245);
+            this.lbl_B.Name = "lbl_B";
+            this.lbl_B.Size = new System.Drawing.Size(80, 80);
+            this.lbl_B.TabIndex = 11;
+            this.lbl_B.Text = "B";
+            this.lbl_B.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_B.Click += new System.EventHandler(this.lbl_B_Click);
+            // 
+            // lbl_Painel
+            // 
+            this.lbl_Painel.BackColor = System.Drawing.Color.DarkOrange;
+            this.lbl_Painel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Painel.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Painel.Location = new System.Drawing.Point(5, 406);
+            this.lbl_Painel.Name = "lbl_Painel";
+            this.lbl_Painel.Size = new System.Drawing.Size(1194, 228);
+            this.lbl_Painel.TabIndex = 12;
+            this.lbl_Painel.Text = "C D E F G A B";
+            this.lbl_Painel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bt_buscar
+            // 
+            this.bt_buscar.Location = new System.Drawing.Point(1205, 545);
+            this.bt_buscar.Name = "bt_buscar";
+            this.bt_buscar.Size = new System.Drawing.Size(127, 86);
+            this.bt_buscar.TabIndex = 13;
+            this.bt_buscar.Text = "Buscar";
+            this.bt_buscar.UseVisualStyleBackColor = true;
+            this.bt_buscar.Click += new System.EventHandler(this.bt_buscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 643);
+            this.Controls.Add(this.bt_buscar);
+            this.Controls.Add(this.lbl_Painel);
+            this.Controls.Add(this.lbl_B);
+            this.Controls.Add(this.lbl_As);
+            this.Controls.Add(this.lbl_A);
+            this.Controls.Add(this.lbl_Gs);
+            this.Controls.Add(this.lbl_G);
+            this.Controls.Add(this.lbl_Fs);
+            this.Controls.Add(this.lbl_F);
+            this.Controls.Add(this.lbl_E);
+            this.Controls.Add(this.lbl_D);
+            this.Controls.Add(this.lbl_Ds);
+            this.Controls.Add(this.lbl_Cs);
+            this.Controls.Add(this.lbl_C);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_C;
+        private System.Windows.Forms.Label lbl_Cs;
+        private System.Windows.Forms.Label lbl_Ds;
+        private System.Windows.Forms.Label lbl_D;
+        private System.Windows.Forms.Label lbl_E;
+        private System.Windows.Forms.Label lbl_F;
+        private System.Windows.Forms.Label lbl_Fs;
+        private System.Windows.Forms.Label lbl_G;
+        private System.Windows.Forms.Label lbl_Gs;
+        private System.Windows.Forms.Label lbl_A;
+        private System.Windows.Forms.Label lbl_As;
+        private System.Windows.Forms.Label lbl_B;
+        private System.Windows.Forms.Label lbl_Painel;
+        private System.Windows.Forms.Button bt_buscar;
+    }
+}
+
---
+++ Escalator/Escalator/Form1.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Escalator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] escalas = {     "CDEFGAB",     "OMFISLC",     "DEIGABO",    "MFGSLCD",    "EISABOM", "FGALCDE",     "ISLBOMF",     "GABCDEI", "SLCOMFG"
+        , "ABODEIS",    "LCDMFGA",     "BOMEISL"};
+        // C#= O , D#= M , F#= I , G#= S , A#= L
+        string[] escalasNome = { "Dó Maior", "Dó Sus. Maior", "Ré Maior", "Ré Sus. Maior", "Mi Maior", "Fá Maior", "Fá Sus. Maior", "Sol Maior", "Sol Sus. Maior"
+        , "Lá Maior", "Lá Sus. Maior", "Si Maior"};
+        bool C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B;
+        int cont=0;
+
+        private void lbl_C_Click(object sender, EventArgs e)
+        {
+            if (lbl_C.BackColor == Color.Plum)
+            {
+                lbl_C.BackColor = Color.Green;
+                C = true;
+                cont = cont + 1;
+            }
+            else
+            {
+                lbl_C.BackColor = Color.Plum;
+                C = false;
+                cont--;
+            }
+        }
+
+        private void lbl_Cs_Click(object sender, EventArgs e)
+        {
+            if (lbl_Cs.BackColor == Color.Plum)
+            {
+                lbl_Cs.BackColor = Color.Green;
+                Cs = true;
+                cont = cont + 1;
+            }
+            else
+            {
+                lbl_Cs.BackColor = Color.Plum;
+                Cs = false;
+                cont--;
+            }
+        }
+
+        private void lbl_D_Click(object sender, EventArgs e)
+        {
+            if (lbl_D.BackColor == Color.Plum)
+            {
+                lbl_D.BackColor = Color.Green;
+                D = true;
+                cont = cont + 1;
+            }
+            else
+            {
+                lbl_D.BackColor = Color.Plum;
+                D = false;
+                cont--;
+            }
+        }
+
+        private void lbl_Ds_Click(object sender, EventArgs e)
+        {
+            if (lbl_Ds.BackColor == Color.Plum)
+            {
+                lbl_Ds.BackColor = Color.Green;
+                Ds = true;
+                cont = cont + 1;
+            }
+            else
+            {
+                lbl_Ds.BackColor = Color.Plum;
+                Ds = false;
+                cont--;
+            }
+        }
+
+        private void lbl_E_Click(object sender, EventArgs e)
+        {
+            if (lbl_E.BackColor == Color.Plum)
+            {
+                lbl_E.BackColor = Color.Green;
+                E = true;
+                cont = cont + 1;
+
+            }
+            else
+            {
+                lbl_E.BackColor = Color.Plum;
+                E = false;
+                cont--;
+            }
+        }
+
+        private void lbl_F_Click(object sender, EventArgs e)
+        {
+            if (lbl_F.BackColor == Color.Plum)
+            {
+                lbl_F.BackColor = Color.Green;
+                F = true;
+                cont = cont + 1;
+            }
+            else
+            {
+                lbl_F.BackColor = Color.Plum;
+                F = false;
+                cont--;
+            }
+        }
+
+        private void lbl_Fs_Click(object sender, EventArgs e)
+        {
+            if (lbl_Fs.BackColor == Color.Plum)
+            {
+                lbl_Fs.BackColor = Color.Green;
+                Fs = true;
+                cont = cont + 1;
+            }
+            else
+            {
+                lbl_Fs.BackColor = Color.Plum;
+                Fs = false;
+                cont--;
+            }
+        }
+
+        private void lbl_G_Click(object sender, EventArgs e)
+        {
+            if (lbl_G.BackColor == Color.Plum)
+            {
+                lbl_G.BackColor = Color.Green;
+                G = true;
+                cont = cont + 1;
+            }
+            else
+            {
+                lbl_G.BackColor = Color.Plum;
+                G = false;
+                cont--;
+            }
+        }
+
+        private void lbl_Gs_Click(object sender, EventArgs e)
+        {
+            if (lbl_Gs.BackColor == Color.Plum)
+            {
+                lbl_Gs.BackColor = Color.Green;
+                Gs = true;
+                cont = cont + 1;
+            }
+            else
+            {
+                lbl_Gs.BackColor = Color.Plum;
+                Gs = false;
+                cont--;
+            }
+        }
+
+        private void lbl_A_Click(object sender, EventArgs e)
+        {
+            if (lbl_A.BackColor == Color.Plum)
+            {
+                lbl_A.BackColor = Color.Green;
+                A = true;
+                cont = cont + 1;
+
+            }
+            else
+            {
+                lbl_A.BackColor = Color.Plum;
+                A = false;
+                cont--;
+            }
+        }
+
+        private void lbl_As_Click(object sender, EventArgs e)
+        {
+            if (lbl_As.BackColor == Color.Plum)
+            {
+                lbl_As.BackColor = Color.Green;
+                As = true;
+                cont = cont + 1;
+
+            }
+            else
+            {
+                lbl_As.BackColor = Color.Plum;
+                As = false;
+                cont--;
+            }
+        }
+
+        private void lbl_B_Click(object sender, EventArgs e)
+        {
+            if (lbl_B.BackColor == Color.Plum)
+            {
+                lbl_B.BackColor = Color.Green;
+                B = true;
+                cont = cont + 1;
+            }
+            else
+            {
+                lbl_B.BackColor = Color.Plum;
+                B = false;
+                cont--;
+            }
+        }
+
+        private void bt_buscar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(cont.ToString());
+            if (cont > 7)
+            {
+                MessageBox.Show("Insira uma quantidade de notas Menor que 7");
+            }
+            else if(cont <= 0)
+            {
+                MessageBox.Show("Insira uma quantidade de notas maior que 0");
+            }
+            else
+            {
+                string[] escalaDigitada = Juntar(C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B);
+                float[] porcen = new float[12];
+                for(int p=0; p < 12; p++)
+                {
+                    porcen[p] = (testador(escalaDigitada, escalas[p]) * 14.285714285714285714285714285714f);
+                    if (porcen[p] == 100)
+                    {
+                        string[] traducao = traduzir(escalaDigitada);
+                        lbl_Painel.Text = traducao[0] + traducao[1] + traducao[2] + traducao[3] + traducao[4] + traducao[5] + traducao[6] + traducao[7] + traducao[8] + traducao[9] + traducao[10] + traducao[11];
+                    }
+                    else { 
+                        lbl_Painel.Text = "???????";
+                         }
+
+                }
+                MessageBox.Show("Nome: "+ escalasNome[0] + " | Chance de: "+ porcen[0].ToString()+"%\n" +
+                    "Nome: " + escalasNome[1] + " | Chance de: " + porcen[1].ToString() + "%\n" +
+                    "Nome: " + escalasNome[2] + " | Chance de: " + porcen[2].ToString() + "%\n" +
+                    "Nome: " + escalasNome[3] + " | Chance de: " + porcen[3].ToString() + "%\n" +
+                    "Nome: " + escalasNome[4] + " | Chance de: " + porcen[4].ToString() + "%\n" +
+                    "Nome: " + escalasNome[5] + " | Chance de: " + porcen[5].ToString() + "%\n" +
+                    "Nome: " + escalasNome[6] + " | Chance de: " + porcen[6].ToString() + "%\n" +
+                    "Nome: " + escalasNome[7] + " | Chance de: " + porcen[7].ToString() + "%\n" +
+                    "Nome: " + escalasNome[8] + " | Chance de: " + porcen[8].ToString() + "%\n" +
+                    "Nome: " + escalasNome[9] + " | Chance de: " + porcen[9].ToString() + "%\n" +
+                    "Nome: " + escalasNome[10] + " | Chance de: " + porcen[10].ToString() + "%\n" +
+                    "Nome: " + escalasNome[11] + " | Chance de: " + porcen[11].ToString() + "%\n");
+            }
+        }
+
+        static string[] Juntar(bool C, bool Cs, bool D, bool Ds, bool E, bool F, bool Fs, bool G, bool Gs, bool A, bool As, bool B)
+        {
+            string[] juntar = new string[12];
+
+            switch (C)
+            {
+                case true:
+                    juntar[0] = "C";
+                    break;
+                case false:
+                    juntar[0] = "x";
+                    break;
+            }
+            switch(Cs)
+            {
+                case true:
+                    juntar[1] = "O";
+                    break;
+                    case false :
+                    juntar[1] = "x";
+                    break;
+            }
+            switch(D)
+            {
+                case true:
+                    juntar[2] = "D";
+                    break;
+                case false:
+                    juntar[2] = "x";
+                    break;
+            }
+            switch (Ds)
+            {
+                case true :
+                    juntar[3] = "M";
+                    break;
+                case false:
+                    juntar[3] = "x";
+                    break;
+            }
+            switch (E)
+            {
+                case true:
+                    juntar[4] = "E";
+                    break;
+                case false:
+                    juntar[4] = "x";
+                    break;
+            }
+            switch (F)
+            {
+                case true:
+                    juntar[5] = "F";
+                    break;
+                case false:
+                    juntar[5] = "x";
+                    break;
+            }
+            switch (Fs)
+            {
+                case true:
+                    juntar[6] = "I";
+                    break;
+                case false:
+                    juntar[6] = "x";
+                    break;
+            }
+            switch (G)
+            {
+                case true:
+                    juntar[7] = "G";
+                    break;
+                case false:
+                    juntar[7] = "x";
+                    break;
+            }
+            switch (Gs)
+            {
+                case true:
+                    juntar[8] = "S";
+                    break;
+                case false:
+                    juntar[8] = "x";
+                    break;
+            }
+            switch (A)
+            {
+                case true:
+                    juntar[9] = "A";
+                    break;
+                case false:
+                    juntar[9] = "x";
+                    break;
+            }
+            switch (As)
+            {
+                case true:
+                    juntar[10] = "L";
+                    break;
+                case false:
+                    juntar[10] = "x";
+                    break;
+            }
+            switch (B)
+            {
+                case true:
+                    juntar[11] = "B";
+                    break;
+                case false:
+                    juntar[11] = "x";
+                    break;
+            }
+
+            return juntar;
+        }
+
+        static int testador(string[] escalaDigitada, string escalaBase)
+        {
+            int seme = 0;
+            for(int i = 0; i < escalaDigitada.Length; i++)
+            {
+                if (escalaBase.Contains(escalaDigitada[i])==true)
+                {
+                    seme++; 
+                }
+            }
+            return seme;
+        }
+        static string[] traduzir(string[] escalaDigitada)
+        {
+            string[] traduzido = new string[escalaDigitada.Length];
+
+            for(int q = 0;q < escalaDigitada.Length; q++)
+            {
+                switch (escalaDigitada[q])
+                {
+                    case "x":
+                        traduzido[q] = "";
+                        break;
+
+                    case "O":
+                        traduzido[q] = "C#";
+                        break;
+                    case "M":
+                        traduzido[q] = "D#";
+                        break;
+                    case "I":
+                        traduzido[q] = "F#";
+                        break;
+                    case "S":
+                        traduzido[q] = "G#";
+                        break;
+                    case "L":
+                        traduzido[q] = "A#";
+                        break;
+
+                    default:
+                        traduzido[q] = escalaDigitada[q];
+                        break;
+                }
+            }
+            return traduzido;
+        }
+    }
+}
